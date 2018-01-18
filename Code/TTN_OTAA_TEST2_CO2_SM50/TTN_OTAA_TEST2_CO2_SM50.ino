f2cf4ac7231a3473bec49c57d2bba157aa8fee69
ssalin <ssalin@pdx.edu>
2018-01-17 16:15:27 -0800

    Serial connection fix
    removed line causing code to wait for a serial connection, which was used to debug SPI speed problems

--- Code/TTN_OTAA_TEST2_CO2_SM50/TTN_OTAA_TEST2_CO2_SM50.ino
+++ Code/TTN_OTAA_TEST2_CO2_SM50/TTN_OTAA_TEST2_CO2_SM50.ino
@@ -169,7 +169,6 @@ void do_send(osjob_t* j){
 void setup() {
     delay(10000);
     Serial.begin(9600);
-    while(!Serial){}
     Serial.println(F("Starting"));
     pinMode(PPMPIN, INPUT); // PPM
     PPM.val = 1000;
